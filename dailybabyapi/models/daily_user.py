6673ae0ddc085c763a2a33c70a2129df39e4faa5
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 16:46:57 -0600

    adds imports for foreign keys

--- dailybabyapi/models/daily_user.py
+++ dailybabyapi/models/daily_user.py
@@ -2,12 +2,14 @@ from dailybabyapi.models.photos import Photo
 from django.db import models
 
 from django.contrib.auth.models import User
+from .photos import Photo
+from datetime import timezone
 
 
 class DailyUser(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
     text_time = models.TimeField(auto_now=False, auto_now_add=False, default=timezone.now)
     phone_number = models.CharField(max_length=15)
-    profile_image = models.ForeignKey(Photo, on_delete=models.DO_NOTHING, null=True)
+    profile_image = models.ForeignKey("Photo", on_delete=models.DO_NOTHING, null=True)
     
