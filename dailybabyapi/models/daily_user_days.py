ffcb6e827d29c1c2ce7a573bedad6a1946be7428
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 16:47:18 -0600

    adds imports for foreign keys

--- dailybabyapi/models/daily_user_days.py
+++ dailybabyapi/models/daily_user_days.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from .daily_user import DailyUser
+from .days_of_week import DayOfWeek
+
 class DailyUserDay(models.Model):
     user = models.ForeignKey("DailyUser", on_delete=models.CASCADE)
     day = models.ForeignKey("DayOfWeek", on_delete=models.CASCADE)
