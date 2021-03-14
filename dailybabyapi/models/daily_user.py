9d9ae88b00051e6c71cef8cc3b37a829cc887e86
Kristen Steele <ksteelephoto@gmail.com>
2021-03-14 17:09:07 -0500

    update profile_image model field to cloudinaryfield

--- dailybabyapi/models/daily_user.py
+++ dailybabyapi/models/daily_user.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from .photos import Photo
 from datetime import timezone
+from cloudinary.models import CloudinaryField
+
 
 
 class DailyUser(models.Model):
@@ -11,5 +13,5 @@ class DailyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     text_time = models.TimeField(auto_now=False, auto_now_add=False)
     phone_number = models.CharField(max_length=15)
-    profile_image = models.ForeignKey("Photo", on_delete=models.DO_NOTHING, null=True)
+    profile_image = CloudinaryField('profile_image')
     
