6916ca2fbe4c0c9191806ea854a00cd4ac8bff66
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 22:31:33 -0600

    update entry character limit

--- dailybabyapi/models/entries.py
+++ dailybabyapi/models/entries.py
@@ -5,5 +5,5 @@ class Entry(models.Model):
     user_baby = models.ForeignKey("UserBaby", on_delete=models.CASCADE)
     prompt = models.ForeignKey("Prompt", on_delete=models.NULL, null=True)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
-    text = models.CharField(max_length=5000)
+    text = models.CharField(max_length=1500)
     is_private = models.BooleanField(default=None)
