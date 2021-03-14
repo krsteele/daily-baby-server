044635bf9d1b1897d5ff0d1b64b11d212b311e1c
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 21:48:55 -0600

    removes unneeded imports

--- dailybabyapi/models/daily_user_days.py
+++ dailybabyapi/models/daily_user_days.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from .daily_user import DailyUser
-from .days_of_week import DayOfWeek
 
 class DailyUserDay(models.Model):
     user = models.ForeignKey("DailyUser", on_delete=models.CASCADE)
