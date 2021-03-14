fe463a86c67c2066c1d7e5d36ad7c9d6a803dcdf
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 21:48:18 -0600

    adds Entry model

---
+++ dailybabyapi/models/entries.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Entry(models.Model):
+    user_baby = models.ForeignKey("UserBaby", on_delete=models.CASCADE)
+    prompt = models.ForeignKey("Prompt", on_delete=models.NULL, null=True)
+    created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
+    text = models.CharField(max_length=5000)
+    is_private = models.BooleanField(default=None)
