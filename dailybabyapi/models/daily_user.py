f2f016164618e85a8e1477dc02ae221bceaf409e
Kristen Steele <ksteelephoto@gmail.com>
2021-03-13 23:08:10 -0600

    migrate models

--- dailybabyapi/models/daily_user.py
+++ dailybabyapi/models/daily_user.py
@@ -8,8 +8,8 @@ from datetime import timezone
 
 class DailyUser(models.Model):
 
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
-    text_time = models.TimeField(auto_now=False, auto_now_add=False, default=timezone.now)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    text_time = models.TimeField(auto_now=False, auto_now_add=False)
     phone_number = models.CharField(max_length=15)
     profile_image = models.ForeignKey("Photo", on_delete=models.DO_NOTHING, null=True)
     
--- dailybabyapi/models/entries.py
+++ dailybabyapi/models/entries.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Entry(models.Model):
     user_baby = models.ForeignKey("UserBaby", on_delete=models.CASCADE)
-    prompt = models.ForeignKey("Prompt", on_delete=models.NULL, null=True)
+    prompt = models.ForeignKey("Prompt", on_delete=models.SET_NULL, null=True)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
     text = models.CharField(max_length=1500)
     is_private = models.BooleanField(default=None)
