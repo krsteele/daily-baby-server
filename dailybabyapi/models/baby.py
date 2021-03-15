8f66b6953f050e8c1fbbbd0799a747b0a97b9e2f
Kristen Steele <ksteelephoto@gmail.com>
2021-03-15 14:06:27 -0500

    adds default profile image

--- dailybabyapi/models/baby.py
+++ dailybabyapi/models/baby.py
@@ -10,4 +10,4 @@ class Baby(models.Model):
     last_name = models.CharField(max_length=25)
     nickname = models.CharField(max_length=25)
     birth_date = models.DateField(default="0000-00-00",)
-    profile_image = CloudinaryField('profile_image')
+    profile_image = CloudinaryField('profile_image', default="https://res.cloudinary.com/fluffydaydream/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1615834269/blank-profile-picture-973460_640_rtmmdv.png")
