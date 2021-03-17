8cd40c7d9504d403d52bd89941d88f60386fb62b
Kristen Steele <ksteelephoto@gmail.com>
2021-03-16 20:16:35 -0500

    adds paths and routes for login_user, register_user and profile

--- daily_baby_server/urls.py
+++ daily_baby_server/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
+from rest_framework import routers
 from django.urls import path
+from dailybabyapi.views import Profile
+
+from dailybabyapi.views import register_user, login_user
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'profile', Profile, 'profile')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
+
