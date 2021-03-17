c87606afb280938a4a0b858817454700a254c576
Kristen Steele <ksteelephoto@gmail.com>
2021-03-17 11:02:12 -0500

    adds relationships.py to views, Relationship ViewSet with list function

---
+++ dailybabyapi/views/relationships.py
@@ -0,0 +1,28 @@
+"""View module for handling requests about relationships"""
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from dailybabyapi.models import Relationship
+
+class Relationship(ViewSet):
+    
+    def list(self, request):
+        """Handle GET requests to relationship resource
+
+        Returns:
+            Response -- JSON serialized list of relationships
+        """
+
+        # Get all relationship records from the database
+        relationships = Relationship.objects.all()
+
+        serializer = RelationshipSerializer(
+            relationships, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+class RelationshipSerializer(serializers.ModelSerializer):
+    """JSON serializer for relationships"""
+    class Meta:
+        model = Relationship
+        fields = ('id', 'relationship')
