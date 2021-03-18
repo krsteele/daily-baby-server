ac8360f0fdf05397812fa9ab46926a1ab45f8f56
Kristen Steele <ksteelephoto@gmail.com>
2021-03-17 20:16:17 -0500

    adds filter and delete for DailyUserDays instances belonging to user, creates new instances with data sent from client

--- dailybabyapi/views/user.py
+++ dailybabyapi/views/user.py
@@ -4,11 +4,19 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from dailybabyapi.models import DailyUser
+from dailybabyapi.models import DailyUserDay
+from dailybabyapi.models import DayOfWeek
 
 
 class Users(ViewSet):
     
     def update(self, request, pk=None):
+        """Handle PUT operations
+
+            Returns:
+                Response -- 204_NO_CONTENT
+        """
+
         dailyUser = DailyUser.objects.get(user=request.auth.user)
         dailyUser.user.first_name = request.data["firstName"]
         dailyUser.user.last_name = request.data["lastName"]
@@ -21,11 +29,18 @@ class Users(ViewSet):
         dailyUser.user.save()
         dailyUser.save()
 
-        # I'll have seven checkboxes with the days of the week. How do I make sure that 
-        # if they uncheck a box, that the DailyUserDays join table instance is deleted? 
-        # I can iterate through the list and create an instance for days of the week that 
-        # checked, but how do I make sure to find and delete the others?
+        DailyUserDay.objects.filter(user=dailyUser).delete()
 
+        listOfDays = request.data["daysOfWeek"]
+        
+        for day in listOfDays:
+            chosenDay = DayOfWeek.objects.get(pk=day)
+            userDay = DailyUserDay()
+            userDay.day = chosenDay
+            userDay.user = dailyUser
+            userDay.save()
+
+ 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
         
 
