a497b5adce873f45927443481a3d5138c167ef4e
Kristen Steele <ksteelephoto@gmail.com>
2021-03-18 11:17:31 -0500

    adds seed.sh

---
+++ seed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf dailybabyapi/migrations
+rm db.sqlite3
+python3 manage.py migrate
+python3 manage.py makemigrations dailybabyapi
+python3 manage.py migrate dailybabyapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata days_of_week
+python3 manage.py loaddata daily_users
+python3 manage.py loaddata daily_user_days
+python3 manage.py loaddata prompts
+python3 manage.py loaddata relationships
+python3 manage.py loaddata babies
+python3 manage.py loaddata user_babies
+python3 manage.py loaddata entries
+python3 manage.py loaddata comments
+python3 manage.py loaddata photos
