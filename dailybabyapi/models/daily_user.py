4aed3a4b6a702be5f86fa70cffa35f0bf367ef88
Kristen Steele <ksteelephoto@gmail.com>
2021-03-22 14:13:02 -0500

    changes cloudinary fields to charfield to accept a url string

--- dailybabyapi/models/daily_user.py
+++ dailybabyapi/models/daily_user.py
@@ -13,5 +13,5 @@ class DailyUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     text_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
     phone_number = models.CharField(max_length=15)
-    profile_image = CloudinaryField('profile_image', default="https://res.cloudinary.com/fluffydaydream/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1615834269/blank-profile-picture-973460_640_rtmmdv.png")
+    profile_image = models.CharField(max_length=200)
     
