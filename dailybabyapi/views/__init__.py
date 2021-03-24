0914dbd438789c5d82818c8fd71eade8dc04ad39
Kristen Steele <ksteelephoto@gmail.com>
2021-03-24 15:41:33 -0500

    removes daysOfWeek from views/init

--- dailybabyapi/views/__init__.py
+++ dailybabyapi/views/__init__.py
@@ -1,7 +1,6 @@
 from .auth import login_user
 from .auth import register_user
 from .profile import Profile
-from .daysOfWeek import DaysOfWeek
 from .relationships import RelationshipView
 from .baby import BabyView
 from .user import Users
