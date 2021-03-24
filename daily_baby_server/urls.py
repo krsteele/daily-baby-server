bbad43e2829b4269427c4c4f0cd82aba1398e1cd
Kristen Steele <ksteelephoto@gmail.com>
2021-03-24 15:42:39 -0500

    removes DaysOfWeek import from urls

--- daily_baby_server/urls.py
+++ daily_baby_server/urls.py
@@ -21,7 +21,6 @@ from django.urls import path
 
 from dailybabyapi.views import register_user, login_user
 from dailybabyapi.views import Profile
-from dailybabyapi.views.daysOfWeek import DaysOfWeek
 from dailybabyapi.views.relationships import RelationshipView
 from dailybabyapi.views.baby import BabyView
 from dailybabyapi.views.user import Users
