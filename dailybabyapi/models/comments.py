2cf3d1e5769d6a03962bfbf054e60f0f398519da
Kristen Steele <ksteelephoto@gmail.com>
2021-03-24 14:04:29 -0500

    update to comments model -- changes length of content field from 200 to 500

--- dailybabyapi/models/comments.py
+++ dailybabyapi/models/comments.py
@@ -4,4 +4,4 @@ class Comment(models.Model):
     entry = models.ForeignKey("Entry", on_delete=models.CASCADE)
     user = models.ForeignKey("DailyUser", on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=500)
