8e29e5015c518da84737df43bc30e1240e1dfed0
Kristen Steele <ksteelephoto@gmail.com>
2021-04-30 07:38:19 -0500

    adds text_event model

--- dailybabyapi/models/text_event.py
+++ dailybabyapi/models/text_event.py
@@ -1,4 +1,18 @@
 # blueprint for a text event
+from django.db import models
+
+
+class TextEvent(models.Model):
+    user = models.ForeignKey("DailyUser", on_delete=models.CASCADE)
+    message_body = models.CharField(max_length=140)
+    phone_number = models.CharField(max_length=15)
+    execution_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    status = models.CharField(max_length=15)
+
+
+
+
+
 
 # user_id
 # message_body
