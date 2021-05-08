83dbe990307db8a6b34ca84bf5816e97143a18a0
Kristen Steele <ksteelephoto@gmail.com>
2021-05-07 22:57:12 -0500

    update databases to postgresql in settings.py

--- daily_baby_server/settings.py
+++ daily_baby_server/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'daily_baby_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dailybaby_db',
+        'USER': 'krsteele',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
