5f0668ab8485d27e95e85598dc241fbfeca0551b
Kristen Steele <ksteelephoto@gmail.com>
2021-05-11 21:01:49 -0500

    adds default callable for days_of_week arrayfield

--- dailybabyapi/models/daily_user.py
+++ dailybabyapi/models/daily_user.py
@@ -7,9 +7,12 @@ from django.contrib.auth.models import User
 
 class DailyUser(models.Model):
 
+    def days_default():
+        return [0, 0, 0, 0, 0, 0, 0]
+        
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     text_time = models.TimeField(auto_now=False, auto_now_add=False)
     phone_number = models.CharField(max_length=15)
     profile_image = models.CharField(max_length=200)
-    days_of_week = ArrayField(models.IntegerField(), size=7,)
+    days_of_week = ArrayField(models.IntegerField(), size=7, default=days_default)
     
